a56d0d110399e8802df2d65a05520f9744f9d48e
YorwanOff <61004826+YorwanOff@users.noreply.github.com>
2020-03-06 09:39:57 +0100

    questions

--- Appli/src/models/Character.php
+++ Appli/src/models/Character.php
@@ -9,4 +9,15 @@ class Character extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'character';
     protected $primaryKey = 'id';
+
+    function game(){
+        return $this->belongsToMany('Game', 'game2character', 'character_id', 'game_id')->get();
+    }
+
+    function findById($id){
+        $pers = $this->game()->where('id','=', $id)->get();
+        foreach ($pers as $key => $value){
+            echo $value['name']." ".$value['deck'];
+        }
+    }
 }
