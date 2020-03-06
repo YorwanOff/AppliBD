d3f89204e4b33b57aab01c0852b1a36c65ee4728
loloznogood <loic.weber3@etu.univ-lorraine.fr>
2020-03-06 09:44:24 +0100

    update question

--- Appli/src/models/Company.php
+++ Appli/src/models/Company.php
@@ -12,4 +12,17 @@ class Company  extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'company';
     protected $primaryKey = 'id';
+
+    /*
+     * Q2 : Liste des compagnies installées au Japon
+     */
+    function q2($pays)
+    {
+        $listeComp = Company::where('location_country', '=', $pays)->get();
+        $res = "";
+        foreach ($listeComp as $comp) {
+            $res .= $comp->name . "<br/>";
+        }
+        return $res;
+    }
 }
