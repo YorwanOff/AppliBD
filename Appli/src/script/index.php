dbc6be38ccd66369e511db52f41b30c17ce34994
YorwanOff <61004826+YorwanOff@users.noreply.github.com>
2020-03-19 16:07:47 +0100

    questions seanxce 3

--- Appli/src/script/index.php
+++ Appli/src/script/index.php
@@ -36,8 +36,10 @@ $platform->q3(10000000);
 echo "<h2>QUESTION 1</h2>";
 $g = new \applibd\models\Game();
 $g->characterByGame(12342);
-*/
+
 /*q2*/
+
+
 /**
 echo "<h2>QUESTION 2</h2>";
 foreach(Game::where('name', 'like', 'Mario%')->get() as $game) {
@@ -64,18 +66,22 @@ foreach(Game::where('name','like','%Mario%')->get() as $game) {
 }*/
 /**
 echo "<h2>QUESTION 5</h2>";
-$game = Game::has('id','>',3)->where('name', 'like', '%Mario%')->get();
-foreach ($game as $g){
-    echo '----' . $game->name . "<br/>";
+$game = Game::where('name', 'like', '%Mario%')->get();
+foreach($game->character()->all() as $c){
+    echo $c->name;
 }
-*/
+
+/**foreach ($game as $g){
+    echo '----' . $game->name . "<br/>";
+}*/
+
 /**
 echo "<h2>QUESTION 6</h2>";
 foreach(Game::where('name', 'like', '%Mario%')->get() as $game){
     foreach ($game->original_game_ratings()->where('name', '=', 'PEGI: 3+')->all() as $r){
         echo '--- ' . $game->name . "<br/>";
     }
-}*/
+}
 
 echo "<h2>QUESTION 7</h2>";
 foreach(Game::where('name', 'like', 'Mario%')->get() as $game){
@@ -85,4 +91,62 @@ foreach(Game::where('name', 'like', 'Mario%')->get() as $game){
             echo '--- ' . $game->name . "<br/>";
         }
     }
+}*/
+echo "<h2>QUESTION 1</h2>";
+$start = microtime(true);
+//$games = Game::get();
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
+echo "<h2>QUESTION 2</h2>";
+$start = microtime(true);
+$games = Game::where('name','like','%Mario%')->get();
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
+echo "<h2>QUESTION 3</h2>";
+$start = microtime(true);
+foreach(Game::where('name', 'like', 'Mario%')->get() as $game){
+    foreach($game->character()->all() as $c){
+        $name = $c;
+    }
 }
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
+echo "<h2>QUESTION 4</h2>";
+$start = microtime(true);
+foreach(Game::where('name', 'like', 'Mario%')->get() as $game){
+    foreach ($game->original_game_ratings()->where('name', '=', 'PEGI: 3+')->all() as $r){
+        $name = $game->name;
+    }
+}
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
+echo "<h2>QUESTION 5</h2>";
+echo "<h3>Première valeur</h3>";
+$start = microtime(true);
+$game = Game::where('name', 'like', 'Mario%')->get();
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
+echo "<h3>Deuxième valeur</h3>";
+$start = microtime(true);
+$game = Game::where('name', 'like', 'Fire Emblem%')->get();
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
+echo "<h3>Troisième valeur</h3>";
+$start = microtime(true);
+$game = Game::where('name', 'like', 'Need%')->get();
+$end = microtime(true);
+$tmp = $end - $start;
+echo "Time : ".$tmp;
+
